f40c01bd8c923deb68539e4abea12dbffd94e70f
Atharv28x <83866870+Atharv28x@users.noreply.github.com>
2021-11-19 22:12:44 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 function startClassification()
 {
     navigator.mediaDevices.getUserMedia({ audio:true});
-    classifier = ml5.soundClassifier('', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/SkI0AGM8j/model.json', modelReady);
 }
 
 function modelReady()
